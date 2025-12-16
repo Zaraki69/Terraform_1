ba2595fcb2e1bbce704f1253a3d275a8feca2da5
Ubuntu <ubuntu@ip-172-31-8-58.ap-south-1.compute.internal>
2025-12-16 13:28:37 +0000

    ec2 instance

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  default = "ami-0a91cd140a1fc148a" // Ubuntu in Mumbai region
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
